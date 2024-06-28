f95d90b6f4ac885081d35d34ba53324aa8f0b8b2
Kevin Sutandi <kevins050324@gmail.com>
2024-06-28 23:35:21 +1000

    added modal

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -6,25 +6,34 @@ import {
 } from '@heroicons/react/24/outline';
 import ButtonAddEvent from './ButtonAddEvent';
 
+import { useState } from 'react';
+import NewEntryModal from './NewEntryModal';
+
 const Sidebar = () => {
+    const [open, setOpen] = useState<boolean>(false);
+
+
 
     return (
-        <div className='fixed flex flex-col w-64 bg-white items-center h-screen shadow-xl rounded-r-xl pt-10'>
-            <div className='flex flex-col w-full items-center gap-1'>
-                <div className="text-center font-extralight py-4 text-2xl tracking-wide">
-                    Mental <br /> Aquarium
+        <>
+            <NewEntryModal open={open} close={() => setOpen(false)} />
+            <div className='fixed flex flex-col w-64 bg-white items-center h-screen shadow-xl rounded-r-xl pt-10'>
+                <div className='flex flex-col w-full items-center gap-1'>
+                    <div className="text-center font-extralight py-4 text-2xl tracking-wide">
+                        Mental <br /> Aquarium
+                    </div>
                 </div>
-            </div>
 
-            <div className='flex flex-col w-full gap-10 mt-14 mb-10'>
-                <SidebarButton href='/' Icon={<HomeIcon />} text='Home' />
-                <SidebarButton href='/courses' text='Journal' Icon={<BookOpenIcon />} />
-                <div className='flex justify-center w-full mt-8'>
-                    <ButtonAddEvent />
+                <div className='flex flex-col w-full gap-10 mt-14 mb-10'>
+                    <SidebarButton href='/' Icon={<HomeIcon />} text='Home' />
+                    <SidebarButton href='/courses' text='Journal' Icon={<BookOpenIcon />} />
+                    <div className='flex justify-center w-full mt-8'>
+                        <ButtonAddEvent open={open} setOpen={setOpen} />
+                    </div>
                 </div>
+                <Profile />
             </div>
-            <Profile />
-        </div>
+        </>
     );
 };
 
