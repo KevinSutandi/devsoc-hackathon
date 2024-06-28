4bd9978786dc3dfe0b77457ea01c007dd89646fb
Edrick Koda <z5349845@unsw.edu.au>
2024-06-28 20:48:08 +1000

    added reset password

--- backend/src/models/auth.models.ts
+++ backend/src/models/auth.models.ts
@@ -19,3 +19,44 @@ export const dbAddUser = async (email: string, password: string) => {
     });
 };
 
+export const dbSetResetToken = async (email: string, resetToken: string) => {
+    try {
+        await prisma.user.update({
+            where: {
+                email: email,
+            },
+            data: {
+                resetToken: resetToken,
+            },
+        });
+    } catch (error) {
+        console.log(error);
+        throw new Error("An database error occurred");
+    }
+}
+
+export const dbSetNewPassword = async (resetToken: string, newPassword: string) => {
+    try {
+        await prisma.user.update({
+            where: {
+                resetToken: resetToken,
+            },
+            data: {
+                password: newPassword,
+                resetToken: null,
+            },
+        })
+        console.log("updated password")
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const dbFindUserByResetToken = async (resetToken: string) => {
+    return await prisma.user.findFirst({
+        where: {
+            resetToken: resetToken,
+        },
+    });
+};
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,7 +3,7 @@ import validator from "validator";
 import { sign } from "jsonwebtoken";
 import { validateName, validatePassword } from "../utils/auth.utils";
 import { sha256 } from "js-sha256";
-import { dbAddUser, dbFindUserByEmail } from "../models/auth.models";
+import { dbAddUser, dbFindUserByEmail, dbFindUserByResetToken, dbSetNewPassword, dbSetResetToken } from "../models/auth.models";
 
 const router = express.Router();
 
@@ -56,9 +56,98 @@ router.post("/register", async (req, res) => {
     return res.status(500).send(error);
   }
 
-
   // Send a success message
   res.status(200).send("User registered");
+});
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    
+    if (!email || !password) {
+        return res.status(400).send("Some details are missing");
+    }
+    
+    // Check if the email is already in use
+    try {
+        const profile = await dbFindUserByEmail(email);
+        if (!profile) {
+        return res.status(400).send("Invalid email or password");
+        }
+        if (profile.password !== sha256(password)) {
+        return res.status(400).send("Invalid email or password");
+        }
+    
+        // Makes the token and sends it to the user
+        const jwtUser: JwtUser = { id: profile.id, email: profile.email };
+        if (!process.env.JWT_HASH) {
+        throw new Error("JWT_HASH not defined");
+        }
+        const token = sign(jwtUser, process.env.JWT_HASH, { expiresIn: "1d" });
+        res.cookie("token", token);
+    } catch (error) {
+        return res.status(500).send("Server error");
+    }
+    
+    // Send a success message
+    res.status(200).send("User logged in");
+});
+
+router.post("/reset-password", async (req, res) => {
+    // Gets the email from the request body
+    const { email } = req.body;
+
+    // Check if the email is wrong
+    if (!email || !validator.isEmail(email)) {
+        return res.status(400).send("Email is required");
+    }
+
+    // Check if the email exists in the database
+    const user = await dbFindUserByEmail(email);
+    if (!user) {
+        return res.status(400).send("Email is not registered");
+    }
+
+    // Generate a reset token
+    const resetToken = sha256((Math.random() + 1).toString(36).substring(7));
+
+    // TODO send an email with a reset link
+    // Setting it in the db
+    try {
+        await dbSetResetToken(email, resetToken);
+    } catch (error) {
+        console.log("setting reset token issue");
+        return res.status(500).send("Server error");
+    }
+    console.log("Reset token: " + resetToken);
+
+    res.status(200).send("Reset link sent");
+});
+
+router.post("/change-password", async (req, res) => {
+    const { resetToken, password } = req.body;
+
+    if (!resetToken || !password) {
+        return res.status(400).send("Reset token and password are required");
+    }
+
+    const hashedPassword = sha256(password);
+
+    console.log("reseting password: ", resetToken, hashedPassword);
+
+    // TODO check the reset token equals the one in the db
+    try {
+        const user = await dbFindUserByResetToken(resetToken);
+
+        if (!user) {
+            return res.status(400).send("Invalid reset token");
+        }
+
+        await dbSetNewPassword(resetToken, hashedPassword);
+    } catch (error) {
+        console.log("setting new password issue");
+        return res.status(500).send("Server error");
+    }
+    res.status(200).send("Password changed");
 });
 
 export default router;
