f9d6fa8cb83dd7c81828f48c71fd324aafb3af68
Edrick Koda <z5349845@unsw.edu.au>
2024-06-29 15:59:46 +1000

    stub calendar route

---
+++ backend/src/routes/calendar.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+export default router;
