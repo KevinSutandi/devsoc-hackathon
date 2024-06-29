ff57bf4d10a2b15ccd064e9f9750451ba7047e85
Hans Kristian Reynaldi <z5348378@ad.unsw.edu.au>
2024-06-29 17:19:00 +1000

    finished checklist

---
+++ frontend/src/components/Checkbox.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { CheckIcon } from "@heroicons/react/24/outline";
+
+interface CustomCheckboxProps {
+  id: string;
+  checked: boolean;
+  onChange: () => void;
+  label: string;
+}
+
+const Checkbox: React.FC<CustomCheckboxProps> = ({
+  id,
+  checked,
+  onChange,
+  label,
+}) => {
+  return (
+    <div className="flex items-center gap-x-3 justify-start">
+      <div
+        className="w-5 h-5 border border-black flex justify-center items-center"
+        onClick={onChange}
+      >
+        <div>{checked && <CheckIcon className="w-5 h-5 animate-fadeIn" />}</div>
+      </div>
+      <div className="w-[80%]">{label}</div>
+    </div>
+  );
+};
+
+export default Checkbox;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,42 @@
 import React, { useEffect, useState } from "react";
 import ButtonEmoji from "../components/ButtonEmoji";
+import { PlusIcon } from "@heroicons/react/24/outline";
+import Checkbox from "../components/Checkbox";
+
+interface ChecklistItem {
+  id: string;
+  label: string;
+  checked: boolean;
+}
 
 const Home: React.FC = () => {
   const [emoji, setEmoji] = useState<string>("");
+
+  const [items, setItems] = useState<ChecklistItem[]>([
+    {
+      id: "1",
+      label:
+        "Complete project documentatiodddsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssd",
+      checked: false,
+    },
+    { id: "2", label: "Review code", checked: false },
+    { id: "3", label: "Deploy to production", checked: false },
+    { id: "4", label: "Complete project documentation", checked: false },
+    { id: "5", label: "Review code", checked: false },
+    { id: "6", label: "Deploy to production", checked: false },
+    { id: "7", label: "Complete project documentation", checked: false },
+    { id: "8", label: "Review code", checked: false },
+    { id: "9", label: "Deploy to production", checked: false },
+  ]);
+
+  const handleToggle = (id: string) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, checked: !item.checked } : item,
+      ),
+    );
+  };
+
   const emojis = ["😊", "😐", "😕", "👿", "😰", "😂"];
 
   const handleChosenEmoji = (emoji: string) => {
@@ -25,11 +59,10 @@ const Home: React.FC = () => {
 
           <div className="w-[30%] flex flex-col justify-center items-center gap-y-[3%]">
             <div className="flex-1 w-full flex flex-col rounded-2xl items-center bg-green-50 shadow-md">
-              <div className="w-full h-[25%] self flex justify-center pt-[7%]">
-                <div className="text-lg font-semibold">
-                  How are you feeling today?
-                </div>
-              </div>
+              <h2 className="text-lg font-semibold w-full h-[25%] self flex justify-center pt-[7%]">
+                How are you feeling today?
+              </h2>
+
               <div className="mt-1 grid grid-cols-3 gap-x-5 space-y-3 w-full h-3/4 justify-center items-center pb-4">
                 {emojis.map((emoji) => (
                   <ButtonEmoji
@@ -40,8 +73,26 @@ const Home: React.FC = () => {
               </div>
             </div>
 
-            <div className="flex-1 w-full flex rounded-2xl justify-center items-center bg-yellow-50 shadow-md">
-              Container 3
+            <div className="flex-1 w-full flex flex-col rounded-2xl bg-yellow-50 shadow-md">
+              <div className="w-full h-[25%] self flex justify-between items-center px-8">
+                <h2 className="text-lg font-semibold">Today's Checklist</h2>
+                <button className="h-6 w-6 border rounded-2xl border-black flex justify-center items-center hover:bg-black/15 hover:border-black/15 hover:duration-200 duration-200">
+                  <PlusIcon className="h-4 w-4" />
+                </button>
+              </div>
+
+              <div className="mx-[9%] flex flex-col gap-y-2 overflow-y-scroll mb-[9%] no-scrollbar overflow-x-scroll">
+                {items.map((item) => (
+                  <div>
+                    <Checkbox
+                      id={item.id}
+                      checked={item.checked}
+                      label={item.label}
+                      onChange={() => handleToggle(item.id)}
+                    />
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
