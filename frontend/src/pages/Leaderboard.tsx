d1c7773fc991170493a81a17c375ff56693aa58f
Edrick Koda <z5349845@unsw.edu.au>
2024-06-30 01:38:39 +1000

    finished Leaderboard

--- frontend/src/pages/Leaderboard.tsx
+++ frontend/src/pages/Leaderboard.tsx
@@ -5,12 +5,21 @@ export default function Leaderboard() {
   const [userOne, setUserOne] = useState("User 1");
   const [userTwo, setUserTwo] = useState("User 2");
   const [userThree, setUserThree] = useState("User 3");
+  const [userRank, setUserRank] = useState(-1);
+  const [allUsers, setAllUsers] = useState([]);
 
   useEffect(() => {
-    axiosInstanceWithAuth.get("/leaderboard").then((res) => {
-      setUserOne(res.data[0].username);
-      setUserTwo(res.data[1].username);
-      setUserThree(res.data[2].username);
+    axiosInstanceWithAuth.get("/leaderboard/top").then((res) => {
+      if (res.data.length <= 0) return;
+      if (res.data.length <= 1) setUserOne(res.data[0].fullname);
+      if (res.data.length <= 2) setUserTwo(res.data[1].fullname);
+      if (res.data.length <= 3) setUserThree(res.data[2].fullname);
+    });
+    axiosInstanceWithAuth.get("/leaderboard/").then((res) => {
+      setUserRank(res.data);
+    });
+    axiosInstanceWithAuth.get("/leaderboard/all").then((res) => {
+      setAllUsers(res.data);
     });
   }, []);
 
@@ -19,25 +28,27 @@ export default function Leaderboard() {
       <div className="w-full h-[70%] flex justify-center items-end">
         <div className="bg-blue-500 h-[45%] w-48 border border-black flex justify-center">
           <div className="w-20 h-20 bg-gray-500 rounded-full flex justify-center items-center font-bold text-2xl">
-            2
+            {userTwo}
           </div>
         </div>
         <div className="bg-blue-500 h-[75%] w-48 border border-black flex justify-center">
           <div className="w-20 h-20 bg-yellow-500 rounded-full flex justify-center items-center font-bold text-2xl">
-            1
+            {userOne}
           </div>
         </div>
         <div className="bg-blue-500 h-[30%] w-48 border border-black flex justify-center">
           <div className="w-20 h-20 bg-orange-700 rounded-full flex justify-center items-center font-bold text-2xl">
-            3
+            {userThree}
           </div>
         </div>
       </div>
 
       <div className="flex h-10 flex-col">
-        <div className="">You are in 3rd position</div>
+        <div className="">
+          You are in {userRank} position out of {allUsers.length} users.
+        </div>
         <div className="flex gap-10">
-          <div>Name</div>
+          <div>Full Name</div>
           <div>Happiness Point</div>
         </div>
       </div>
