3cd79935a7c745d196219ecbf21c63262a843918
Edrick Koda <z5349845@unsw.edu.au>
2024-06-29 16:23:32 +1000

    routes are completed

--- backend/src/routes/calendar.ts
+++ backend/src/routes/calendar.ts
@@ -1,12 +1,38 @@
 import express from "express";
+import { dbGetMonthByUid, dbUpsertCalendar } from "../models/calendar.models";
+import { CustomRequest } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/month", async (req, res) => {
+    const { date } = req.query;
     try {
-        
+        const customReq = req as CustomRequest;
+        if (!customReq.token || typeof customReq.token === "string") {
+            throw new Error("Token is not valid");
+        }
+
+        return await dbGetMonthByUid(
+            customReq.token.uid,
+            new Date(date as string),
+        );
     } catch (error) {
-        
+        return res.status(500).send("Server error");
+    }
+});
+
+router.post("/log", async (req, res) => {
+    try {
+        const { date, mood } = req.body;
+
+        const customReq = req as CustomRequest;
+        if (!customReq.token || typeof customReq.token === "string") {
+            throw new Error("Token is not valid");
+        }
+
+        return await dbUpsertCalendar(date, customReq.token.uid, mood);
+    } catch (error) {
+        return res.status(500).send("Server error");
     }
 });
 
