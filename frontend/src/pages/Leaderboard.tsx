ec8bde9f36d36d014fa38c35de64a253e43e343e
Edrick Koda <z5349845@unsw.edu.au>
2024-06-30 11:34:35 +1000

    added points display

--- frontend/src/pages/Leaderboard.tsx
+++ frontend/src/pages/Leaderboard.tsx
@@ -5,26 +5,35 @@ export default function Leaderboard() {
   const [userOne, setUserOne] = useState("User 1");
   const [userTwo, setUserTwo] = useState("User 2");
   const [userThree, setUserThree] = useState("User 3");
+  const [userOnePoints, setUserOnePoints] = useState(0);
+  const [userTwoPoints, setUserTwoPoints] = useState(0);
+  const [userThreePoints, setUserThreePoints] = useState(0);
   const [userRank, setUserRank] = useState(-1);
   const [userPoints, setUserPoints] = useState(0);
   const [allUsers, setAllUsers] = useState([]);
 
   useEffect(() => {
     axiosInstanceWithAuth.get("/leaderboard/top").then((res) => {
+      console.log(res.data);
       if (res.data.length > 0) {
         setUserOne(res.data[0].fullname);
+        setUserOnePoints(res.data[0].happinessPoints);
+        console.log(userOnePoints);
       }
       if (res.data.length > 1) {
         setUserTwo(res.data[1].fullname);
+        setUserTwoPoints(res.data[1].happinessPoints);
       }
       if (res.data.length > 2) {
         setUserThree(res.data[2].fullname);
+        setUserThreePoints(res.data[2].happinessPoints);
       }
     });
     axiosInstanceWithAuth.get("/leaderboard/").then((res) => {
       setUserRank(res.data.rank);
       setUserPoints(res.data.happinessLevel);
     });
+    console.log(userThreePoints);
     axiosInstanceWithAuth.get("/leaderboard/all").then((res) => {
       setAllUsers(res.data);
     });
@@ -41,19 +50,37 @@ export default function Leaderboard() {
         </div>
       </div>
       <div className="w-full h-[70%] flex justify-center items-end">
-        <div className="bg-blue-500 h-[45%] w-48 border border-black flex justify-center">
+        <div
+          className="bg-blue-500 h-[45%] w-48 border border-black flex flex-col items-center
+         justify-start"
+        >
           <div className="w-20 h-20 bg-gray-500 rounded-full flex justify-center items-center font-bold text-2xl">
             {userTwo}
           </div>
+          <div className="text-yellow-500 text-3xl">
+            {userTwoPoints} Points !!
+          </div>
         </div>
-        <div className="bg-blue-500 h-[75%] w-48 border border-black flex justify-center">
+        <div
+          className="bg-blue-500 h-[75%] w-48 border border-black flex flex-col items-center
+         justify-start"
+        >
           <div className="w-20 h-20 bg-yellow-500 rounded-full flex justify-center items-center font-bold text-2xl">
             {userOne}
           </div>
+          <div className="text-yellow-500 text-3xl">
+            {userOnePoints} Points !!
+          </div>
         </div>
-        <div className="bg-blue-500 h-[30%] w-48 border border-black flex justify-center">
+        <div
+          className="bg-blue-500 h-[30%] w-48 border border-black flex flex-col items-center
+         justify-start"
+        >
           <div className="w-20 h-20 bg-orange-700 rounded-full flex justify-center items-center font-bold text-2xl">
             {userThree}
+          </div>
+          <div className="text-yellow-500 text-3xl">
+            {userOnePoints} Points !!
           </div>
         </div>
       </div>
